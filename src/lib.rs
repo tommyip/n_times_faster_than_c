1facfed9760918743f688f13dc2139edbcd00484
Thomas Ip <thomas@ipthomas.com>
2023-07-11 14:44:05 +0100

    Rewrite to be more readable

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use rand::{distributions::Bernoulli, prelude::Distribution, thread_rng};
 use seq_macro::seq;
-use std::arch::aarch64::{vaddlvq_u8, vaddq_u8, vld1q_dup_u8, vld1q_u8, vmovq_n_u8, vqsubq_u8};
+use std::arch::aarch64::{vaddlvq_u8, vaddq_u8, vandq_u8, vld1q_u8, vmovq_n_u8};
 
 pub fn baseline(input: &str) -> i64 {
     let mut res = 0;
@@ -43,27 +43,22 @@ pub fn opt2_count_s(input: &str) -> i64 {
 }
 
 pub fn opt3_count_s_branchless(input: &str) -> i64 {
-    let n_s = input
-        .bytes()
-        .map(|b| b.saturating_sub(b's' - 1) as i64)
-        .sum::<i64>();
+    let n_s = input.bytes().map(|b| (b & 1) as i64).sum::<i64>();
     (2 * n_s) - input.len() as i64
 }
 
 pub fn opt4_simd(input: &str) -> i64 {
     let n = input.len();
     const N_LANES: usize = 16;
-    let n_blocks = n / N_LANES;
-    let n_fast = n_blocks * N_LANES;
+    let n_iters = n / N_LANES;
+    let n_simd_elems = n_iters * N_LANES;
     let mut res = 0;
     unsafe {
-        // saturating subtract ('s' - 1) from s/p
-        // so we get 1 for s, 0 for p
         let mut acc_v = vmovq_n_u8(0);
-        let sub_v = vld1q_dup_u8(&(b's' - 1));
-        for block_i in 0..n_blocks {
+        let one_v = vmovq_n_u8(1);
+        for block_i in 0..n_iters {
             let input_v = vld1q_u8(input[block_i * N_LANES..].as_ptr());
-            let eq_s_v = vqsubq_u8(input_v, sub_v);
+            let eq_s_v = vandq_u8(input_v, one_v);
             acc_v = vaddq_u8(acc_v, eq_s_v);
             if block_i % (u8::MAX as usize + 1) == u8::MAX as usize {
                 res += vaddlvq_u8(acc_v) as i64;
@@ -72,8 +67,8 @@ pub fn opt4_simd(input: &str) -> i64 {
         }
         res += vaddlvq_u8(acc_v) as i64;
     }
-    res = (2 * res) - n_fast as i64;
-    res + baseline_bytes(&input[n_fast..])
+    res = (2 * res) - n_simd_elems as i64;
+    res + baseline_bytes(&input[n_simd_elems..])
 }
 
 macro_rules! simd_unrolled {
@@ -82,19 +77,19 @@ macro_rules! simd_unrolled {
             let n = input.len();
             const N_LANES: usize = 16;
             const N_ELEM_PER_ITER: usize = N_LANES * $unroll_factor;
-            let n_blocks = n / N_ELEM_PER_ITER;
-            let n_fast = n_blocks * N_ELEM_PER_ITER;
+            let n_iters = n / N_ELEM_PER_ITER;
+            let n_simd_elems = n_iters * N_ELEM_PER_ITER;
             let mut res = 0;
             unsafe {
                 seq!(I in 0..$unroll_factor {
                     let mut v_acc~I = vmovq_n_u8(0);
                 });
-                let sub_v = vld1q_dup_u8(&(b's' - 1));
-                for block_i in 0..n_blocks {
+                let one_v = vmovq_n_u8(1);
+                for block_i in 0..n_iters {
                     let offset = block_i * N_LANES * $unroll_factor;
                     seq!(I in 0..$unroll_factor {
                         let v_input~I = vld1q_u8(input[offset + I * N_LANES..].as_ptr());
-                        let v_eq_s~I= vqsubq_u8(v_input~I, sub_v);
+                        let v_eq_s~I= vandq_u8(v_input~I, one_v);
                         v_acc~I = vaddq_u8(v_acc~I, v_eq_s~I);
                     });
                     if block_i % (u8::MAX as usize + 1) == u8::MAX as usize {
@@ -108,8 +103,8 @@ macro_rules! simd_unrolled {
                     res += vaddlvq_u8(v_acc~I) as i64;
                 });
             }
-            res = (2 * res) - n_fast as i64;
-            res + baseline_bytes(&input[n_fast..])
+            res = (2 * res) - n_simd_elems as i64;
+            res + baseline_bytes(&input[n_simd_elems..])
         }
     }
 }
@@ -134,36 +129,33 @@ pub fn gen_random_input(size: usize) -> String {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_correctness() {
-        let input = "spssssp";
-        let expected = 3;
-        assert_eq!(expected, baseline(input));
-        assert_eq!(expected, baseline_bytes(input));
-        assert_eq!(expected, opt1_idiomatic(input));
-        assert_eq!(expected, opt2_count_s(input));
-        assert_eq!(expected, opt3_count_s_branchless(input));
-        assert_eq!(expected, opt4_simd(input));
-        assert_eq!(expected, opt5_simd_unrolled_2x(input));
-        assert_eq!(expected, opt5_simd_unrolled_4x(input));
-        assert_eq!(expected, opt5_simd_unrolled_8x(input));
-        assert_eq!(expected, opt5_simd_unrolled_10x(input));
-        assert_eq!(expected, opt5_simd_unrolled_16x(input));
+    macro_rules! assert_eq_all {
+        ($expected:expr, $input:expr) => {
+            assert_eq!($expected, baseline($input));
+            assert_eq!($expected, baseline_bytes($input));
+            assert_eq!($expected, opt1_idiomatic($input));
+            assert_eq!($expected, opt2_count_s($input));
+            assert_eq!($expected, opt3_count_s_branchless($input));
+            assert_eq!($expected, opt4_simd($input));
+            assert_eq!($expected, opt5_simd_unrolled_2x($input));
+            assert_eq!($expected, opt5_simd_unrolled_4x($input));
+            assert_eq!($expected, opt5_simd_unrolled_8x($input));
+            assert_eq!($expected, opt5_simd_unrolled_10x($input));
+            assert_eq!($expected, opt5_simd_unrolled_16x($input));
+        };
     }
 
     #[test]
-    fn test_correctness_large() {
+    fn test_simple() {
+        assert_eq_all!(3, "spssssp");
+        assert_eq_all!(100, &(0..100).into_iter().map(|_| 's').collect::<String>());
+        assert_eq_all!(-100, &(0..100).into_iter().map(|_| 'p').collect::<String>());
+    }
+
+    #[test]
+    fn test_large() {
         let input = gen_random_input(421337);
         let expected = baseline(&input);
-        assert_eq!(expected, baseline_bytes(&input));
-        assert_eq!(expected, opt1_idiomatic(&input));
-        assert_eq!(expected, opt2_count_s(&input));
-        assert_eq!(expected, opt3_count_s_branchless(&input));
-        assert_eq!(expected, opt4_simd(&input));
-        assert_eq!(expected, opt5_simd_unrolled_2x(&input));
-        assert_eq!(expected, opt5_simd_unrolled_4x(&input));
-        assert_eq!(expected, opt5_simd_unrolled_8x(&input));
-        assert_eq!(expected, opt5_simd_unrolled_10x(&input));
-        assert_eq!(expected, opt5_simd_unrolled_16x(&input));
+        assert_eq_all!(expected, &input);
     }
 }
